c7047752d724216cb96843928b89774f7fb86ceb
bijayg7 <112832092+bijayg7@users.noreply.github.com>
2024-05-08 15:38:42 +1000

    Some updates in layout

---
+++ Error.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Error() {
+	return (
+        <div className="error-container">
+			<div className="error-body">
+				<h1>
+					Error Occured...
+				</h1>
+				<h3>
+					Please try again later.
+				</h3>
+			</div>
+		</div>
+	);
+}
+
+export default Error;
